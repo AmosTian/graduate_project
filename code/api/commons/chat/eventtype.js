d4cb5d52c3e56440ed314a79ac8548d2ad385d0b
Auspice Tian <42266788+AuspiceTian@users.noreply.github.com>
2021-05-29 18:08:23 +0800

    毕设完成

--- code/api/commons/chat/eventtype.js
+++
@@ -1,5 +0,0 @@
-// 3. 定义事件类型
-module.exports = {
-  CONNECTED: Symbol("connected"),
-  CLOSED: Symbol("closed")
-}
--- code/api/config/config-pro.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  env: "production",
-  /* 日志开关负责是否打印日志 */
-  logging: false
-}
